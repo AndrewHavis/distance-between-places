16bff00dccf2cd96f09fff9767d5f5a96daab793
Andrew Havis <andrew@andrew-havis.co.uk>
2016-07-30 11:23:57 +0100

    Got the distance calculation working in metres

--- dev/js/controllers/dbp.ctrl.js
+++ dev/js/controllers/dbp.ctrl.js
@@ -7,4 +7,23 @@ app.controller('dbpCtrl', ['$scope', function($scope) {
     $scope.place1 = null;
     $scope.place2 = null;
     $scope.distance = 0;
+    $scope.latLong = [];
+    
+    console.log($scope);
+    
+    // Get the latitude and longitude of our two places
+    $scope.calculateDistance = function () {
+        if ($scope.place1 !== null) {
+            console.log($scope.place1);
+            $scope.latLong[0] = new google.maps.LatLng($scope.place1.geometry.location.lat(), $scope.place1.geometry.location.lng());
+        }
+        if ($scope.place2 !== null) {
+            console.log($scope.place2);
+            $scope.latLong[1] = new google.maps.LatLng($scope.place2.geometry.location.lat(), $scope.place1.geometry.location.lng());
+        }
+        if ($scope.place1 !== null && $scope.place2 !== null) {        
+            // Now get the two sets of coordinates and calculate the distance between them using Google Maps' geometry library
+            $scope.distance = google.maps.geometry.spherical.computeDistanceBetween($scope.latLong[0], $scope.latLong[1]);
+        }
+    };
 }]);
